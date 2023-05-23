f7bbf7b1998b639e6d71fa04786d60ccb4b78d7e
Aria S.p.a <info@ariaspa.it>
2023-05-23 13:01:45 +0200

    Automated Release of version 1.22.2

--- src/migrations/m220725_147900_fix_documenti_titolo_multiple_spaces.php
+++ src/migrations/m220725_147900_fix_documenti_titolo_multiple_spaces.php
@@ -8,31 +8,27 @@
  * @package    svilupposostenibile\enti
  * @category   CategoryName
  */
-
 use yii\db\Migration;
 use open20\amos\documenti\models\Documenti;
 
+class m220725_147900_fix_documenti_titolo_multiple_spaces extends Migration {
 
-class m220725_147900_fix_documenti_titolo_multiple_spaces extends Migration
-{
+    public function up() {
+        $documenti = \Yii::$app->db
+                ->createCommand("SELECT * FROM documenti where deleted_at is null and titolo like '%  %'")
+                ->queryAll();
 
-    public function up()
-    {
-        $documenti = Documenti::find()
-                ->andWhere(['deleted_at'=>null])
-                ->andWhere(['like', 'titolo', '  '])->all();
-
-        foreach($documenti as $doc){
-            $titolo = preg_replace("/ {2,}/", " ", $doc->titolo); 
+        foreach ($documenti as $doc) {
+            $titolo = preg_replace("/ {2,}/", " ", $doc->titolo);
             \Yii::$app->db->createCommand("UPDATE documenti SET titolo =:titolo WHERE id =:id")
                     ->bindValue(':titolo', $titolo)
                     ->bindValue(':id', $doc->id)
-                    ->execute();      
+                    ->execute();
         }
-     
     }
-    public function down()
-    {
+
+    public function down() {
         return true;
     }
+
 }
